3f271fe031cc61f8a6586b86bf775c58dc8b0a6f
liqy <liqingyi@outlook.com>
2018-01-09 15:51:58 +0800

    内涵段子

--- neihan/src/main/java/com/liqy/neihan/MainActivity.java
+++ neihan/src/main/java/com/liqy/neihan/MainActivity.java
@@ -3,6 +3,11 @@ package com.liqy.neihan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 内涵段子首页
+ * 接口
+ * http://lf.snssdk.com/neihan/service/tabs/
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
